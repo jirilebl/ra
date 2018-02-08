855d473a7f268aaf0064b4dd070242e7258b4235
Jiri (George) Lebl <jiri.lebl@gmail.com>
2018-02-08 15:54:57 -0600

    add chapters/sections to stats

--- getstats.sh
+++ getstats.sh
@@ -4,16 +4,22 @@ VOLII="realanal2.tex ch-several-vars-ders.tex ch-one-dim-ints-sv.tex ch-multivar
 
 echo
 echo Volume I
+echo Chapters:"\t"`grep '^[\]chapter\**{' $VOLI | wc -l`
+echo Sections:"\t"`grep '^[\]section\**{' $VOLI | wc -l`
 echo Exercises:"\t"`grep '^[\]begin{exercise' $VOLI | wc -l`
 echo Figures:"\t"`grep '^[\]begin{myfigureht' $VOLI | wc -l`
 
 echo
 echo Volume II 
+echo Chapters:"\t"`grep '^[\]chapter\**{' $VOLII | wc -l`
+echo Sections:"\t"`grep '^[\]section\**{' $VOLII | wc -l`
 echo Exercises:"\t"`grep '^[\]begin{exercise' $VOLII | wc -l`
 echo Figures:"\t"`grep '^[\]begin{myfigureht' $VOLII | wc -l`
 
 echo
 echo Both volumes
+echo Chapters:"\t"`grep '^[\]chapter\**{' $VOLI $VOLII | wc -l`
+echo Sections:"\t"`grep '^[\]section\**{' $VOLI $VOLII | wc -l`
 echo Exercises:"\t"`grep '^[\]begin{exercise' $VOLI $VOLII | wc -l`
 echo Figures:"\t"`grep '^[\]begin{myfigureht' $VOLI $VOLII | wc -l`
 
