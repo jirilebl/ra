5eff9469b8992e1fdd3fdd1f0c70e16093f632ed
Jiri (George) Lebl <jiri.lebl@gmail.com>
2018-02-12 20:16:59 -0600

    for whatever reason automake isn't working on my laptop so add back makeglossaries to publish.sh

--- publish.sh
+++ publish.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # This is really overly anal, but at least 4 runs seem to be needed with
-# the ogx, so better be safe and run it 5 times
-pdflatex realanal
-pdflatex realanal
-pdflatex realanal
-pdflatex realanal
+# the ogx, so better be safe and run it 5 times, also automake in glossaries
+# doesn't seem to always work
+for n in 1 2 3 4; do
+  pdflatex realanal
+  makeindex realanal
+  makeglossaries realanal
+done
 pdflatex realanal
 # We are now using imakeidx and [automake] in glossaries
-# so no need to run these separately
+# so no need to run these separately (FIXME: doesn't seem to work
+# on one of my machines, that's why the run above, I've added makeindex
+# just for good measure there)
