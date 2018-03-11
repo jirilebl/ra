7089e3188801d8299474a25093ae91119f85e234
Jiri (George) Lebl <jiri.lebl@gmail.com>
2018-03-11 14:07:19 -0500

    update stat stript a bit

--- getstats.sh
+++ getstats.sh
@@ -3,24 +3,30 @@ VOLI="realanal.tex ch-vol1-intro.tex ch-real-nums.tex ch-seq-ser.tex ch-contfunc
 VOLII="realanal2.tex ch-several-vars-ders.tex ch-one-dim-ints-sv.tex ch-multivar-int.tex ch-approximate.tex"
 
 echo
-echo Volume I
-echo Chapters:"\t"`grep '^[\]chapter\**{' $VOLI | wc -l`
-echo Sections:"\t"`grep '^[\]section\**{' $VOLI | wc -l`
-echo Exercises:"\t"`grep '^[\]begin{exercise' $VOLI | wc -l`
-echo Figures:"\t"`grep '^[\]begin{myfigureht' $VOLI | wc -l`
+echo "Volume I"
+echo "Chapters:\t\t"`grep '^[\]chapter\**{' $VOLI | wc -l`
+echo "Sections:\t\t"`grep '^[\]section\**{' $VOLI | wc -l`
+echo "Sections \\w exercises:\t"`grep '^[\]subsection{Exercises}' $VOLI | wc -l`
+echo "Exercises:\t\t"`grep '^[\]begin{exercise' $VOLI | wc -l`
+echo "Figures:\t\t"`grep '^[\]begin{myfigureht' $VOLI | wc -l`
 
 echo
-echo Volume II 
-echo Chapters:"\t"`grep '^[\]chapter\**{' $VOLII | wc -l`
-echo Sections:"\t"`grep '^[\]section\**{' $VOLII | wc -l`
-echo Exercises:"\t"`grep '^[\]begin{exercise' $VOLII | wc -l`
-echo Figures:"\t"`grep '^[\]begin{myfigureht' $VOLII | wc -l`
+echo "Volume II"
+echo "Chapters:\t\t"`grep '^[\]chapter\**{' $VOLII | wc -l`
+echo "Sections:\t\t"`grep '^[\]section\**{' $VOLII | wc -l`
+echo "Sections \\w exercises:\t"`grep '^[\]subsection{Exercises}' $VOLII | wc -l`
+echo "Exercises:\t\t"`grep '^[\]begin{exercise' $VOLII | wc -l`
+echo "Figures:\t\t"`grep '^[\]begin{myfigureht' $VOLII | wc -l`
 
 echo
-echo Both volumes
-echo Chapters:"\t"`grep '^[\]chapter\**{' $VOLI $VOLII | wc -l`
-echo Sections:"\t"`grep '^[\]section\**{' $VOLI $VOLII | wc -l`
-echo Exercises:"\t"`grep '^[\]begin{exercise' $VOLI $VOLII | wc -l`
-echo Figures:"\t"`grep '^[\]begin{myfigureht' $VOLI $VOLII | wc -l`
+echo "Both volumes"
+echo "Chapters:\t\t"`grep '^[\]chapter\**{' $VOLI $VOLII | wc -l`
+echo "Sections:\t\t"`grep '^[\]section\**{' $VOLI $VOLII | wc -l`
+echo "Sections \\w exercises:\t"`grep '^[\]subsection{Exercises}' $VOLI $VOLII | wc -l`
+echo "Exercises:\t\t"`grep '^[\]begin{exercise' $VOLI $VOLII | wc -l`
+echo "Figures:\t\t"`grep '^[\]begin{myfigureht' $VOLI $VOLII | wc -l`
+
+echo
+echo Chapter count includes introductions.
 
 echo
