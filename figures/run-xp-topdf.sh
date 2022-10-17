9721c6997092628cb404b79479580d7617f3af1a
Jiri (George) Lebl <jiri.lebl@gmail.com>
2022-10-17 12:02:58 -0500

    Update fonts for the new edition

--- figures/run-xp-topdf.sh
+++ figures/run-xp-topdf.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 # Used from run-all-xp.sh
-exec elaps --pdf -p amsmath -p amsfonts -p amssymb -p mathptmx -p helvet -p nicefrac --size 12 $1
+# Times
+#exec elaps --pdf -p amsmath -p amsfonts -p amssymb -p mathptmx -p helvet -p nicefrac --size 12 $1
+# Palatino
+# FIXME: what about the options?
+exec elaps --pdf -p amsmath -p amsfonts -p amssymb -p newpxtext -p newpxmath -p nicefrac --size 12 $1
